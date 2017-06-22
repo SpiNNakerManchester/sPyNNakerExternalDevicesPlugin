5ec0d0dc5e55c172b55638bff26f7320a9f53703
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-06-22 11:30:28 +0100

    Shortening of names from upstream

--- spynnaker_external_devices_plugin/pyNN/external_devices_models/push_bot/push_bot_parameters/push_bot_led.py
+++ spynnaker_external_devices_plugin/pyNN/external_devices_models/push_bot/push_bot_parameters/push_bot_led.py
@@ -3,7 +3,7 @@ from spynnaker_external_devices_plugin.pyNN\
     import MunichIoSpiNNakerLinkProtocol
 from spynnaker_external_devices_plugin.pyNN.external_devices_models.push_bot\
     .abstract_push_bot_output_device import AbstractPushBotOutputDevice
-from data_specification.enums.data_type import DataType
+from data_specification.enums import DataType
 
 
 class PushBotLED(AbstractPushBotOutputDevice):
