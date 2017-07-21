90432ea07261e7835498a987d238252464c75d57
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-07-21 16:59:04 +0100

    Added in files for backwards compatibility (with warnings)

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from collections import defaultdict
+import os
+
+__version__ = None
+exec(open("spynnaker_external_devices_plugin/_version.py").read())
+assert __version__
+
+# Build a list of all project modules, as well as supplementary files
+main_package = "spynnaker_external_devices_plugin"
+data_extensions = {".aplx", ".xml"}
+main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
+start = len(main_package_dir)
+packages = []
+package_data = defaultdict(list)
+for dirname, dirnames, filenames in os.walk(main_package_dir):
+    if '__init__.py' in filenames:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        packages.append(package)
+    for filename in filenames:
+        _, ext = os.path.splitext(filename)
+        if ext in data_extensions:
+            package = "{}{}".format(
+                main_package, dirname[start:].replace(os.sep, '.'))
+            package_data[package].append("*{}".format(ext))
+            break
+
+url = "https://github.com/SpiNNakerManchester/sPyNNakerExternalDevicesPlugin"
+
+setup(
+    name="sPyNNakerExternalDevicesPlugin",
+    version=__version__,
+    description="Spinnaker External Devices Plugin",
+    url=url,
+    packages=packages,
+    package_data=package_data,
+    install_requires=['sPyNNaker >= 1!4.0.0a5, < 1!5.0.0']
+)
---
+++ spynnaker_external_devices_plugin/pyNN/__init__.py
@@ -0,0 +1,9 @@
+from spynnaker7.pyNN.external_devices import *
+
+import warnings
+
+warnings.warn(
+    "Using sPyNNakerExternalDevicesPlugin in deprecated."
+    " Please instead use sPyNNaker7.pyNN.external_devices"
+    " (or if you have import pyNN.spiNNaker as p in your script you can use"
+    " p.external_devices)")
